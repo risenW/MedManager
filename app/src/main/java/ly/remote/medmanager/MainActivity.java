6348acb3508c2b0add3d22ffbbaff2811c9cb772
Rising Odegua <williamsrising592@gmail.com>
2018-03-29 13:34:49 +0100

    Added two boiler plate activities: About and profile. Modified the card view and added MVC folders.

--- app/src/main/java/ly/remote/medmanager/MainActivity.java
+++ app/src/main/java/ly/remote/medmanager/MainActivity.java
@@ -1,7 +1,10 @@
 package ly.remote.medmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.profile:
+                Intent intent1 = new Intent(this,ProfileActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.about:
+                Intent intent = new Intent(this,About.class);
+                startActivity(intent);
+                break;
+            case R.id.rate:
+                //Google play id goes here
+                break;
+            case R.id.exit:
+                //exit code
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+
+        }
+        return true;
     }
 
 
